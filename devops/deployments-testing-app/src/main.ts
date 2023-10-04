6e18c1c7af9c19b86bca924fc8cdeb012bea41f0
Kevin Towe <kevintowe@Kevins-Air.attlocal.net>
2023-10-03 20:38:08 -0500

    have testing app configured to consume the deployments library;

--- devops/deployments-testing-app/src/main.ts
+++ devops/deployments-testing-app/src/main.ts
@@ -1,1 +1,3 @@
-console.log('Hello World');
+import {initDeployments} from '@vizientinc/deployments'
+
+initDeployments({projectName: 'testingTeam123'})
--- devops/deployments/src/lib/init.ts
+++ devops/deployments/src/lib/init.ts
@@ -1,37 +1,21 @@
-import { Provider } from '@pulumi/pulumi/provider';
 import { DeploymentProvider } from '../types';
 import { Provider as ADO_Provider } from '@pulumi/azuredevops';
-import { ComponentResource } from '@pulumi/pulumi';
 
 type InitConfig = {
-  deploymentProvider: DeploymentProvider;
+//   deploymentProvider: DeploymentProvider;/
   projectName: string;
   uniqueName?: string;
 };
 
-// export const initDeployments = (config: InitConfig) => {
-// 	const personalAccessToken = process.env['AZDO_PERSONAL_ACCESS_TOKEN'];
-//   const orgServiceUrl = process.env['AZDO_ORG_SERVICE_URL'];
+export const initDeployments = (config: InitConfig) => {
+  const personalAccessToken = process.env['AZDO_PERSONAL_ACCESS_TOKEN'];
+  const orgServiceUrl = process.env['AZDO_ORG_SERVICE_URL'];
 
-//   const provider = new ADO_Provider(`name`, { personalAccessToken, orgServiceUrl });
-  
-//   return { config, provider };
-// };
+  const provider = new ADO_Provider(`name`, {
+    personalAccessToken,
+    orgServiceUrl,
+  });
+  console.log(provider)
 
-class MyComponent extends ComponentResource {
-	// public readonly bucket: aws.s3.Bucket | undefined;
-
-	constructor(name: string, args: aws.s3.BucketArgs, opts?: pulumi.ComponentResourceOptions) {
-			super("Vizientinc:DevOps:Deployments", name, {}, opts);
-
-			try {
-					this.bucket = new aws.s3.Bucket(`${name}-bucket`, args, { parent: this });
-			} catch (error) {
-					console.error(`Failed to create the Deployment Init resource.: ${error}`);
-					throw error;
-			}
-	}
-
-export const initDeployments = () => {
-
-}
+  return { config, provider };
+};
